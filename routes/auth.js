8535d7b770495410a42848a1f26fda6da512517e
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-02-12 15:12:26 +0530

    added post and refactored user and note models

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
 
-router.get('/', (req, res) => {
-  res.json({
-    result:1,
-    endpoint:'/auth'
-  })
+router.post('/', (req, res) => {
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports = router
