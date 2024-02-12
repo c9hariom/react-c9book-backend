4236d0e1b34b672582ab4f2e11bd1be4abce1d0c
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-02-12 20:56:31 +0530

    enhanced auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,22 @@ const router = express.Router();
 const User = require('../models/User')
 const {body, validationResult} = require('express-validator')
 
-router.post('/',[
+router.post('/createuser/',[
     body('name','enter valid name').isLength({min:3}),
     body('email','enter valid email').isEmail(),
     body('password','enter valid password').isLength({min:5})
-], (req, res) => {
+],async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
-    User.create({
+
+    let user = await User.findOne({email:req.body.email});
+    if(user){
+        return res.status(400).json({error:'sorry '+user.name+' with this email exists already'});
+    }
+
+    user = await User.create({
         name:req.body.name,
         email:req.body.email,
         password:req.body.password
@@ -21,7 +27,7 @@ router.post('/',[
     }).catch((error)=>{
         console.log("error creating user "+error);
     });
-    res.send("hi")
+    res.json(req.body)
 })
 
 module.exports = router
